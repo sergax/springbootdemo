f92c5d4a0bae6e84f2ad499839ac88ac41bc8d37
sergax <serey.aksenchenko@gmail.com>
2022-01-06 01:19:42 +0200

    Initial commit

---
+++ src/main/java/com/sergax/springbootdemo/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.sergax.springbootdemo.repository;
+
+import com.sergax.springbootdemo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
